f0eeee73d7256c75cc70d2801d5832e8767407e2
tomorin258 <632772086@qq.com>
2025-06-21 20:55:20 +0200

    解决冲突

---
+++ src/main/java/dao/mappers/CommentsMapper.java
@@ -0,0 +1,69 @@
+package dao.mappers;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * The interface CommentsMapper.
+ * Provides CRUD operations for the comments table.
+ * Maps to the 'comments' table in the database.
+ * 
+ * @author: Pan Zitao
+ */
+public interface CommentsMapper {
+
+    /**
+     * Add a comment entry.
+     * @param comments the comments object
+     * @return true if successful, false otherwise
+     */
+    boolean addComments(@Param("comments") Comments comments);
+
+    /**
+     * Delete a comment by its ID.
+     * @param commentId the comment ID
+     * @return true if successful, false otherwise
+     */
+    boolean deleteComments(@Param("commentId") Integer commentId);
+
+    /**
+     * Update a comment entry.
+     * @param comments the comments object
+     * @return true if successful, false otherwise
+     */
+    boolean updateComments(@Param("comments") Comments comments);
+
+    /**
+     * Get a comment by its ID.
+     * @param commentId the comment ID
+     * @return the comments object
+     */
+    Comments getCommentsById(@Param("commentId") int commentId);
+
+    /**
+     * Get all comments for a specific recipe.
+     * @param recipeId the recipe ID
+     * @return a list of comments
+     */
+    ArrayList<Comments> getCommentsByRecipeId(@Param("recipeId") int recipeId);
+
+    /**
+     * Get all comments made by a specific user.
+     * @param userId the user ID
+     * @return a list of comments
+     */
+    ArrayList<Comments> getCommentsByUserId(@Param("userId") int userId);
+
+    /**
+     * Get the newest comment (based on createdAt).
+     * @return the newest comment
+     */
+    Comments getNewestComments();
+
+    /**
+     * Get all comments.
+     * @return a list of all comments
+     */
+    ArrayList<Comments> getAllComments();
+}
---
+++ src/main/java/dao/mappers/IngredientsMapper.java
@@ -0,0 +1,62 @@
+package dao.mappers;
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * The interface IngredientsMapper.
+ * Provides CRUD operations for ingredients table.
+ * 
+ * @author: Pan Zitao
+ */
+
+public interface IngredientsMapper {
+
+    /**
+     * Add an ingredient entry.
+     *
+     * @param ingredients the ingredients object
+     * @return the boolean
+     */
+    boolean addIngredients(@Param("ingredients") Ingredients ingredients);
+
+    /**
+     * Delete an ingredient entry.
+     * @param ingredientsId the ingredients id
+     * @return the boolean
+     */
+    boolean deleteIngredients(@Param("ingredientsId") Integer ingredientsId);
+
+    /**
+     * Update an ingredient entry.
+     * @param ingredients the ingredients object
+     * @return the boolean
+     */
+    boolean updateIngredients(@Param("ingredients") Ingredients ingredients);
+
+    /**
+     * Get ingredient by id.
+     * @param ingredientsId the ingredients id
+     * @return the ingredients
+     * */
+    Ingredients getIngredientsById(@Param("ingredientsId") int ingredientsId);
+
+    /**
+     * Get ingredient by name.
+     * @param name the ingredients name
+     * @return the list of ingredients
+     */
+    ArrayList<Ingredients> getIngredientsByName(@Param("name") String name);
+
+    /**
+     * Get the newest ingredient.
+     * @return the newest ingredient
+     * */
+    Ingredients getNewestIngredients();
+
+    /**
+     * Get all ingredients.
+     * @return the list of all ingredients
+     * */
+    ArrayList<Ingredients> getAllIngredients();
+}
